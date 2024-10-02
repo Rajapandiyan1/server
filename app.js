7c6377f334460f6c6f963f33244b268966dd10e0
Raja Pandiyan <rajapandiyan1163@gmail.com>
2024-10-02 11:55:45 +0530

    origin add

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const {verifyAuthPerson} = require('./Auth/tokenAuthen');
 require('dotenv').config(); 
 
 app.use(cookie());
-  app.use(cors({ origin: 'http://localhost:3000',methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],credentials:true }));
+  app.use(cors({ origin: 'https://myoptions.vercel.app',methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],credentials:true }));
   
 app.use(body.urlencoded({extended:true}));
 app.use(body.json());
